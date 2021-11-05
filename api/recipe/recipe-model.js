283ce6eed946c7e538fcafc826b9a77edc474c6f
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-04 17:03:13 -0700

    added dummy data, first model revision

--- api/recipe/recipe-model.js
+++ api/recipe/recipe-model.js
@@ -1,8 +1,39 @@
 import db from "../../data/db-config";
 
 const getRecipeById = async (recipe_id) => {
-  const recipe = await db("recipe as r");
-  return recipe;
+  const recipe = await db("recipe as r")
+    .leftJoin("step as s",
+      "r.recipe_id", "s.recipe_id")
+    .leftJoin("step_ingredient as si",
+      "s.step_id", "si.step_id")
+    .leftJoin("ingredient as i",
+      "si.ingredient_id", "i.ingredient_id")
+    .select("r.recipe_id",
+      "r.recipe_name",
+      "s.step_id",
+      "s.step_number",
+      "s.step_instruction",
+      "si.ingredient_id",
+      "i.ingredient_name",
+      "si.ingredient_quantity",
+      "i.unit_of_measurement")
+    .where("r.recipe_id", recipe_id)
+    .orderBy("s.step_number", "asc");
+  if (recipe.recipe_id) {
+    return {
+      recipe_id: recipe[0].recipe_id,
+      recipe_name: recipe[0].recipe_name,
+      steps: recipe.map(step => {
+        return {
+          step_id: step.step_id,
+          step_number: step.step_number,
+          step_instruction: step.step_instruction,
+        };
+      })
+    };
+  } else {
+    return recipe;
+  }
 };
 
 export {
--- queries.sql
+++ queries.sql
@@ -10,8 +10,9 @@
         i.unit_of_measurement
    FROM [recipe] AS r
         LEFT JOIN [step] as s
-        ON s.recipe_id = s.recipe_id
+        ON r.recipe_id = s.recipe_id
         LEFT JOIN [step_ingredient] as si
-        ON si.step_id = s.step_id
+        ON s.step_id = si.step_id
         LEFT JOIN [ingredient] as i
-        ON i.ingredient_id = si.ingredient_id;
+        ON si.ingredient_id = i.ingredient_id
+  ORDER BY s.step_number ASC;
